194eff1304022ec826d761f547334c130e6a7827
zju-edu-zj <2943168611@qq.com>
2023-09-13 21:57:26 +0800

    proxy finished

--- src/bin/proxy.rs
+++ src/bin/proxy.rs
@@ -1,25 +1,44 @@
 #![feature(impl_trait_in_assoc_type)]
 
 use std::net::SocketAddr;
+use tracing::debug;
 
 use bdredis::{
     client2proxy::Client2ProxyService,
-    proxy2master::Proxy2MasterService,
-    proxy2slave::Proxy2SlaveService,
+    //proxy2master::Proxy2MasterService,
+    //proxy2slave::Proxy2SlaveService,
 };
     
 use volo_gen::rds::{
     ScServiceServer,
-    ScServiceClient, ScServiceClientBuilder,
+    //ScServiceClient, ScServiceClientBuilder,
 };
+
+use tracing_subscriber::{fmt, util::SubscriberInitExt, prelude::__tracing_subscriber_SubscriberExt};
+
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
 #[volo::main]
 async fn main() {
-    let addr: SocketAddr = "[::]:8080".parse().unwrap();
-    let addr = volo::net::Address::from(addr);
+    tracing_subscriber::registry().with(fmt::layer()).init();
+    let conf_path = "../../config/ms.conf";
+    let mut conf_file = File::open(conf_path).await.unwrap();
 
+    debug!("read configure");
+
+    let mut lines = String::new();
+    let _ = conf_file.read_to_string(&mut lines).await;
+    let mut lines_split = lines.split_whitespace();
+    let master_addr = String::from(lines_split.next().unwrap());
+    //let master_addr: SocketAddr = master_addr.parse().unwrap();
+    let slaves:Vec<&str> = lines_split.collect();
+    let proxy = Client2ProxyService::new(&master_addr,&slaves);
+    
+    let addr: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let addr = volo::net::Address::from(addr);
     // TODO: 不能用await
-    ScServiceServer::new(Client2ProxyService {})
+    ScServiceServer::new(proxy)
         .run(addr)
         .await
         .unwrap();
--- src/client2proxy.rs
+++ src/client2proxy.rs
@@ -1,3 +1,4 @@
+
 // Client2Proxy 
 // Client作为RPC client, Proxy作为RPC server
 use anyhow::anyhow;
@@ -8,28 +9,92 @@ use volo_gen::rds::{
     GetRequest, GetResponse,
     DelRequest, DelResponse,
 };
+use std::net::SocketAddr;//, thread};
+//use crate::proxy2server::Proxy2ServerService;
+use std::hash::{Hash,Hasher};
 
-pub struct Client2ProxyService;
+use std::collections::hash_map::DefaultHasher;
 
+//use crate::proxy2server;
+
+pub struct Client2ProxyService{
+	master: volo_gen::rds::ScServiceClient,
+	slaves: Vec<volo_gen::rds::ScServiceClient>,
+}
+impl Client2ProxyService {
+	pub fn new(master_addr:&str, slaves_addr: &Vec<&str>)->Self{
+		let addr: SocketAddr = master_addr.parse().unwrap();
+        let mas = volo_gen::rds::ScServiceClientBuilder::new("master")
+            .address(addr).build();
+		let mut sla_v:Vec<volo_gen::rds::ScServiceClient> = Vec::new();
+		for slave in slaves_addr{
+			let addr: SocketAddr = slave.parse().unwrap();
+			let mut name = String::from("slave");
+			name.push_str(slave);
+        	let sla = volo_gen::rds::ScServiceClientBuilder::new(name)
+            .address(addr).build();
+			sla_v.push(sla);
+		}
+		Client2ProxyService{
+			master: mas,
+			slaves: sla_v,
+		}
+	}
+	fn my_hash(&self, input:&str)->usize{
+		let mut hasher = DefaultHasher::new();
+		input.hash(&mut hasher);
+		hasher.finish() as usize % self.slaves.len()
+	}
+}
 #[volo::async_trait]
 impl ScService for Client2ProxyService {
 	async fn ping(&self, _req: PingRequest) ->
 		::core::result::Result<PingResponse, ::volo_thrift::AnyhowError> {
-        Err(anyhow!("TODO"))
+
+			match _req.payload {
+				Some(payload) => {
+					Ok(PingResponse { payload })
+				},
+				None => {
+					Ok(PingResponse { payload: "PONG".parse().unwrap() })
+				}
+			}
 	}
 
 	async fn set(&self, _req: SetRequest) ->
-		::core::result::Result<SetResponse, ::volo_thrift::AnyhowError> {
-        Err(anyhow!("TODO"))
+		::core::result::Result<SetResponse, ::volo_thrift::AnyhowError> {  //all to the master
+        match self.master.set(_req).await{
+			Ok(resp) => {
+				Ok(resp)
+			}
+			Err(_) =>{
+				Err(anyhow!("failed"))
+			}
+		}
 	}
 
 	async fn get(&self, _req: GetRequest) ->
 		::core::result::Result<GetResponse, ::volo_thrift::AnyhowError> {
-        Err(anyhow!("TODO"))
+        let distubutor = self.my_hash(&_req.key);
+		match self.slaves[distubutor].get(_req).await{
+			Ok(resp) => {
+				Ok(resp)
+			}
+			Err(_) =>{
+				Err(anyhow!("failed"))
+			}
+		}
 	}
 
 	async fn del(&self, _req: DelRequest) ->
 		::core::result::Result<DelResponse, ::volo_thrift::AnyhowError> {
-        Err(anyhow!("TODO"))
+			match self.master.del(_req).await{
+				Ok(resp) => {
+					Ok(resp)
+				}
+				Err(_) =>{
+					Err(anyhow!("failed"))
+				}
+			}
 	}
 }
