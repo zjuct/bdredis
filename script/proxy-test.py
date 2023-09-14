088f8057cc59be5fed6dcdac5f5ce0c098b4c480
chentong <3210100706@zju.edu.cn>
2023-09-14 20:57:29 +0800

    add test for proxy

---
+++ script/proxy-test.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import os
+import subprocess
+import time
+
+# 启动master-slave
+REDIS_PATH = os.environ["MINIREDIS_PATH"]
+CONF_PATH = REDIS_PATH + "/config/proxy-test.conf"
+EXEC_DIR = REDIS_PATH + "/target/debug"
+LOG_DIR = REDIS_PATH + "/script/proxy-test"
+f = open(CONF_PATH)
+master = True
+lines = f.readlines()
+fnull = open("/dev/null")
+for idx, line in enumerate(lines):
+    flog = open(f'{LOG_DIR}/server{idx}.log', '+w')
+    line = line.strip()
+    bin = f'{EXEC_DIR}/{line}'
+    subprocess.Popen(bin.split(), stderr=fnull, stdout=flog)
+
+    if master:
+        time.sleep(2)
+        master = False
+
+time.sleep(2)
+
+# 启动proxy
+proxy = EXEC_DIR + "/proxy"
+args = ['bash', '-c', proxy]
+f = open("/dev/null")
+subprocess.Popen(args, stderr=fnull, stdout=fnull)
+
+# 启动client
+bin = "cargo test -q --bin client-test proxy_test"
+f = open("/dev/null")
+subprocess.Popen(bin.split(), stderr=fnull, stdout=fnull)
+
+time.sleep(10)
+
+# 统计所有slave接受的get数量
+for i in range(1, len(lines)):
+    p1 = subprocess.Popen(["cat", f"{LOG_DIR}/server{i}.log"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep", "DEBUG"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p3 = subprocess.Popen(["grep", "get"], stdin=p2.stdout, stdout=subprocess.PIPE)
+    p4 = subprocess.Popen(["wc", "-l"], stdin=p3.stdout)
+
